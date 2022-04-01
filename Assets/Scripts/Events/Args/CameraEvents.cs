29ec50311dce7ed1381b53c3eb36632f9d5e7b13
Nico <nicolas-donald.laurent@laposte.net>
2022-04-01 22:20:45 +0200

    Add camera / events / random usefull stuff

---
+++ Assets/Scripts/Events/Args/CameraEvents.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class GetCameraEvent
+{
+    public Camera camera;
+
+    public GetCameraEvent()
+    {
+        camera = null;
+    }
+}
+
+class AddScreenShakeEvent
+{
+    public ScreenShakeBase screenShake;
+    public int resultID;
+
+    public AddScreenShakeEvent(ScreenShakeBase _screenShake)
+    {
+        screenShake = _screenShake;
+    }
+}
+
+class StopScreenShakeEvent
+{
+    public int ID;
+
+    public StopScreenShakeEvent(int id)
+    {
+        ID = id;
+    }
+}
+
+class StopAllScreenShakeEvent { }
---
+++ Assets/Scripts/Events/Args/MoveEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CenterUpdatedEventInstant
+{
+    public Vector3 pos;
+
+    public CenterUpdatedEventInstant(Vector3 _pos)
+    {
+        pos = _pos;
+    }
+}
+
+public class CenterUpdatedEvent
+{
+    public Vector3 pos;
+
+    public CenterUpdatedEvent(Vector3 _pos)
+    {
+        pos = _pos;
+    }
+}
