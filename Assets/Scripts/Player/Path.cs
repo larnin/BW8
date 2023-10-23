ac8f18a1a3c005ed93cb230d2c4ddb6ce07239f3
Nicolas <nicolas-donald.laurent@laposte.net>
2023-10-23 22:52:25 +0200

    WIP paths

--- Assets/Scripts/Player/Path.cs
+++ Assets/Scripts/Player/Path.cs
@@ -7,5 +7,26 @@ using UnityEngine;
 
 public class Path : MonoBehaviour
 {
+    [SerializeField] [HideInInspector] bool m_bInit = false;
+    [SerializeField] List<Vector2> m_points;
 
+    private void OnValidate()
+    {
+        if(!m_bInit)
+        {
+            InitPoints();
+        }
+    }
+
+    void InitPoints()
+    {
+        if (m_points == null)
+            m_points = new List<Vector2>();
+        m_points.Clear();
+
+        m_points.Add(new Vector2(-0.5f, -0.5f));
+        m_points.Add(new Vector2(-0.5f, 0.5f));
+        m_points.Add(new Vector2(0.5f, -0.5f));
+        m_points.Add(new Vector2(0.5f, 0.5f));
+    }
 }
