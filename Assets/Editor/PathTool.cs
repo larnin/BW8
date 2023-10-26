a649104169f52a0098b6ef2075a9c9231fa25e87
Nicolas <nicolas-donald.laurent@laposte.net>
2023-10-26 14:20:36 +0200

    Paths and editor

---
+++ Assets/Editor/PathTool.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+[EditorTool("Path Tool", typeof(Path))]
+public class PathTool : EditorTool, IDrawSelectedHandles
+{
+    [Shortcut("Activate Path Tool", typeof(SceneView), KeyCode.P)]
+    static void PlatformToolShortcut()
+    {
+        if (Selection.GetFiltered<Path>(SelectionMode.TopLevel).Length > 0)
+            ToolManager.SetActiveTool<PathTool>();
+        else
+            Debug.Log("No Path selected!");
+    }
+
+    public override void OnToolGUI(EditorWindow window)
+    {
+        if (!(window is SceneView sceneView))
+            return;
+
+        foreach (var obj in targets)
+        {
+            if (obj is Path path)
+                DrawOnePathHandles(path);
+        }
+    }
+
+    public void OnDrawHandles()
+    {
+        foreach (var obj in targets)
+        {
+            if (obj is Path path)
+                DrawOnePathDebug(path);
+        }
+    }
+
+    void DrawOnePathHandles(Path path)
+    {
+        for (int i = 0; i < path.GetPointNb(); i++)
+        {
+            Handles.color = Color.yellow;
+            Vector3 pos = path.GetPointPos(i);
+
+            EditorGUI.BeginChangeCheck();
+            //pos = Handles.PositionHandle(pos, Quaternion.identity);
+            pos = Handles.Slider2D(pos, Vector3.forward, Vector3.up, Vector3.left, 0.015f, Handles.DotHandleCap, 0.0f);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(path, "Move Path");
+                path.SetPointPos(i, pos);
+            }
+
+            Vector3 orthoDir = path.GetPointOrthoDir(i);
+            float width = path.GetPointWidth(i);
+
+            Vector3 offPos = pos + orthoDir * width;
+            Vector3 offPos2 = pos - orthoDir * width;
+
+            Handles.color = Color.green;
+
+            EditorGUI.BeginChangeCheck();
+            offPos = Handles.Slider2D(offPos, Vector3.forward, Vector3.up, Vector3.left, 0.015f, Handles.DotHandleCap, 0.0f);
+            if (EditorGUI.EndChangeCheck())
+            {
+                float dist = (offPos - pos).magnitude;
+                path.SetPointWidth(i, dist);
+            }
+
+            EditorGUI.BeginChangeCheck();
+            offPos2 = Handles.Slider2D(offPos2, Vector3.forward, Vector3.up, Vector3.left, 0.015f, Handles.DotHandleCap, 0.0f);
+            if (EditorGUI.EndChangeCheck())
+            {
+                float dist = (offPos2 - pos).magnitude;
+                path.SetPointWidth(i, dist);
+            }
+        }
+    }
+
+    void DrawOnePathDebug(Path path)
+    {
+        int nbPoint = path.IsLoop() ? path.GetPointNb() : path.GetPointNb() - 1;
+
+        for (int i = 0; i < nbPoint; i++)
+        {
+            int nextIndex = i < nbPoint - 1 ? i + 1 : 0;
+
+            Vector3 pos = path.GetPointPos(i);
+            Vector3 nextPos = path.GetPointPos(nextIndex);
+
+            Vector3 orthoDir = path.GetPointOrthoDir(i);
+            Vector3 nextOrthoDir = path.GetPointOrthoDir(nextIndex);
+
+            float width = path.GetPointWidth(i);
+            float nextWidth = path.GetPointWidth(nextIndex);
+
+            Handles.color = Color.yellow;
+            Handles.DrawLine(pos, nextPos, 0.5f);
+
+            Handles.color = Color.green;
+
+            Vector3 offPos = pos + orthoDir * width;
+            Vector3 nextOffPos = nextPos + nextOrthoDir * nextWidth;
+            Handles.DrawLine(offPos, nextOffPos, 1.5f);
+
+            Vector3 offPos2 = pos - orthoDir * width;
+            Vector3 nextOffPos2 = nextPos - nextOrthoDir * nextWidth;
+            Handles.DrawLine(offPos2, nextOffPos2, 1.5f);
+
+            Handles.DrawLine(offPos, offPos2, 1.5f);
+            Handles.DrawLine(nextOffPos, nextOffPos2, 1.5f);
+        }
+    }
+}
--- Assets/Scripts/Player/Path.cs
+++ Assets/Scripts/Player/Path.cs
@@ -5,28 +5,148 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
+[Serializable]
+public class PathPoint
+{
+    public Vector2 position;
+    public float width;
+
+    public PathPoint(Vector2 pos, float w)
+    {
+        position = pos;
+        width = w;
+    }
+}
+
 public class Path : MonoBehaviour
 {
-    [SerializeField] [HideInInspector] bool m_bInit = false;
-    [SerializeField] List<Vector2> m_points;
+    [SerializeField] [HideInInspector] bool m_init = false;
+    [SerializeField] bool m_loop = true;
+    [SerializeField] List<PathPoint> m_points;
 
     private void OnValidate()
     {
-        if(!m_bInit)
+        if(!m_init)
         {
             InitPoints();
+            m_init = true;
         }
     }
 
     void InitPoints()
     {
         if (m_points == null)
-            m_points = new List<Vector2>();
+            m_points = new List<PathPoint>();
         m_points.Clear();
 
-        m_points.Add(new Vector2(-0.5f, -0.5f));
-        m_points.Add(new Vector2(-0.5f, 0.5f));
-        m_points.Add(new Vector2(0.5f, -0.5f));
-        m_points.Add(new Vector2(0.5f, 0.5f));
+        m_points.Add(new PathPoint(new Vector2(-0.5f, -0.5f), 0.1f));
+        m_points.Add(new PathPoint(new Vector2(-0.5f, 0.5f), 0.1f));
+        m_points.Add(new PathPoint(new Vector2(0.5f, 0.5f), 0.1f));
+        m_points.Add(new PathPoint(new Vector2(0.5f, -0.5f), 0.1f));
+    }
+
+    public int GetPointNb() { return m_points.Count; }
+
+    public void AddPoint(Vector3 pos, float width = 0.1f)
+    {
+        Vector2 localPos = ToLocal(pos);
+
+        m_points.Add(new PathPoint(localPos, width));
+    }
+
+    public void RemovePoint(int index)
+    {
+        if (index < 0 || index >= m_points.Count)
+            return;
+
+        m_points.RemoveAt(index);
+    }
+
+    public Vector3 GetPointPos(int index)
+    {
+        if (index < 0 || index >= m_points.Count)
+            return transform.position;
+
+        return ToGlobal(m_points[index].position);
+    }
+
+    public void SetPointPos(int index, Vector3 pos)
+    {
+        if (index < 0 || index >= m_points.Count)
+            return;
+
+        m_points[index].position = ToLocal(pos);
+    }
+
+    public float GetPointWidth(int index)
+    {
+        if (index < 0 || index >= m_points.Count)
+            return 0;
+
+        return m_points[index].width;
+    }
+
+    public void SetPointWidth(int index, float width)
+    {
+        if (index < 0 || index >= m_points.Count)
+            return;
+
+        m_points[index].width = width;
+    }
+
+    public Vector3 GetPointOrthoDir(int index)
+    {
+        if (index < 0 || index >= m_points.Count)
+            return Vector3.up;
+
+        if (m_points.Count <= 1)
+            return Vector3.up;
+
+        int lastPointIndex = index == 0 ? m_points.Count - 1 : index - 1;
+        int nextPointIndex = index == m_points.Count - 1 ? 0 : index + 1;
+
+        if(!m_loop)
+        {
+            if (index == 0)
+                lastPointIndex = 1;
+            if (index == m_points.Count - 1)
+                nextPointIndex = index - 1;
+        }
+
+        Vector2 pos = m_points[index].position;
+        Vector2 lastPos = m_points[lastPointIndex].position;
+        Vector2 nextPos = m_points[nextPointIndex].position;
+
+        if(lastPointIndex == nextPointIndex)
+        {
+            Vector2 offset = pos - lastPos;
+            nextPos = pos + offset;
+        }
+
+        Vector2 dir = (lastPos - pos).normalized + (pos - nextPos).normalized;
+
+        Vector3 orthoDir = new Vector3(-dir.y, dir.x, 0);
+        float norm = orthoDir.magnitude;
+        if (norm > 0.001f)
+            orthoDir /= norm;
+
+        return orthoDir;
+    }
+
+    public bool IsLoop() { return m_loop; }
+
+    Vector2 ToLocal(Vector3 pos)
+    {
+        pos -= transform.position;
+        return new Vector2(pos.x, pos.y);
+    }
+
+    Vector3 ToGlobal(Vector2 pos)
+    {
+        Vector3 globalPos = transform.position;
+        globalPos.x += pos.x;
+        globalPos.y += pos.y;
+
+        return globalPos;
     }
 }
