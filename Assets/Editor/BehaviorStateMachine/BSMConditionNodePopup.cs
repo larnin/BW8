11b7bc0abfa35d6829e1f6a38bf46d86851b8ad5
Nicolas <nicolas-donald.laurent@laposte.net>
2023-12-27 14:13:54 +0100

    Starting conditions

---
+++ Assets/Editor/BehaviorStateMachine/BSMConditionNodePopup.cs
@@ -0,0 +1,53 @@
+﻿using NLocalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public class BSMConditionNodePopup : PopupWindowContent
+{
+    string m_filter = "";
+    BSMConditionNode m_node;
+
+    Vector2 m_scrollPos = Vector2.zero;
+
+    public BSMConditionNodePopup(BSMConditionNode node)
+    {
+        m_node = node;
+    }
+
+    public override void OnGUI(Rect rect)
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Filter:", GUILayout.Width(40));
+        m_filter = GUILayout.TextField(m_filter);
+        GUILayout.EndHorizontal();
+
+        var types = typeof(BSMConditionBase).Assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(BSMConditionBase)));
+
+        m_scrollPos = GUILayout.BeginScrollView(m_scrollPos);
+        foreach (var t in types)
+        {
+            if (!Loc.ProcessFilter(t.Name, m_filter))
+                continue;
+
+            if (GUILayout.Button(t.Name))
+            {
+                SetCondition(t);
+                editorWindow.Close();
+            }
+        }
+        GUILayout.EndScrollView();
+    }
+
+    void SetCondition(Type t)
+    {
+        if (m_node == null)
+            return;
+
+        //todo
+    }
+}
--- Assets/Editor/BehaviorStateMachine/Elements/BSMConditionNode.cs
+++ Assets/Editor/BehaviorStateMachine/Elements/BSMConditionNode.cs
@@ -4,7 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEditor.Experimental.GraphView;
+using UnityEditor;
+using UnityEngine.UIElements;
 
 public class BSMConditionNode : BSMNode
 {
+    BSMConditionBase m_condition = null;
+
+    public override void Draw() 
+    {
+        base.Draw();
+
+        if (m_condition == null)
+            DrawSetCondition();
+        else DrawCondition();
+        
+        RefreshExpandedState();
+    }
+
+    void DrawSetCondition()
+    {
+        Button addChoiceButton = BSMUtility.CreateButton("Set condition", CreatePopup);
+        extensionContainer.Add(addChoiceButton);
+    }
+
+    void DrawCondition()
+    {
+
+    }
+
+    public void SetCondition(BSMConditionBase condition)
+    {
+        m_condition = condition;
+    }
+
+    void CreatePopup()
+    {
+        var pos = GetPosition();
+
+        UnityEditor.PopupWindow.Show(pos, new BSMConditionNodePopup(this));
+    }
 }
