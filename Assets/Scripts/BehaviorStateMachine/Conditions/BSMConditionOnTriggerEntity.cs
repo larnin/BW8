4dffcbed1d555622659e9abe514321fbed894ec6
Nico <nicolas-donald.laurent@laposte.net>
2024-02-22 08:02:01 +0100

    Triggers conditions

---
+++ Assets/Scripts/BehaviorStateMachine/Conditions/BSMConditionOnTriggerEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BSMConditionOnTriggerEntity : BSMConditionBase
+{
+    static string triggerName = "Trigger";
+    static string targetName = "Target";
+
+    public BSMConditionOnTriggerEntity()
+    {
+        AddAttribute(triggerName, new BSMAttributeObject(""));
+        AddAttribute(targetName, BSMAttributeObject.Create((GameObject)null));
+    }
+
+    public override bool IsValid()
+    {
+        string trigger = GetStringAttribute(triggerName);
+        var entity = GetUnityObjectAttribute<GameObject>(targetName);
+
+        if (trigger == null || entity == null)
+            return false;
+
+        GetFirstQuestEntityEvent getEntity = new GetFirstQuestEntityEvent(trigger);
+        Event<GetFirstQuestEntityEvent>.Broadcast(getEntity);
+
+        if (getEntity.entity == null)
+            return false;
+
+        var collider = getEntity.entity.GetComponent<Collider2D>();
+        if (collider == null)
+            return false;
+
+        return collider.OverlapPoint(entity.transform.position);
+    }
+}
---
+++ Assets/Scripts/BehaviorStateMachine/Conditions/BSMConditionOnTriggerSelf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BSMConditionOnTriggerSelf : BSMConditionBase
+{
+    static string triggerName = "Trigger";
+
+    public BSMConditionOnTriggerSelf()
+    {
+        AddAttribute(triggerName, new BSMAttributeObject(""));
+    }
+
+    public override bool IsValid()
+    {
+        string trigger = GetStringAttribute(triggerName);
+
+        if (trigger == null)
+            return false;
+
+        GetFirstQuestEntityEvent getEntity = new GetFirstQuestEntityEvent(trigger);
+        Event<GetFirstQuestEntityEvent>.Broadcast(getEntity);
+
+        if (getEntity.entity == null)
+            return false;
+
+        var collider = getEntity.entity.GetComponent<Collider2D>();
+        if (collider == null)
+            return false;
+
+        return collider.OverlapPoint(GetControler().transform.position);
+    }
+}
